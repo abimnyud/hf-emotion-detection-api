b611e8c5196a4dc3d9e2873e3dc41dcce502ac50
Abimanyu <abimanyu.damarjati@gmail.com>
2023-11-26 18:50:49 +0700

    Init

---
+++ index.js
@@ -0,0 +1,57 @@
+import http from 'http';
+import querystring from 'querystring';
+import url from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = http.createServer();
+const hostname = '127.0.0.1';
+const port = 3000;
+
+server.on('request', async (req, res) => {
+    // Parse the request URL
+    const parsedUrl = url.parse(req.url);
+
+    // Extract the query parameters
+    const { text } = querystring.parse(parsedUrl.query);
+
+    // Set the response headers
+    res.setHeader('Content-Type', 'application/json');
+
+    let response;
+    let result = "";
+    if (parsedUrl.pathname === '/classify' && text) {
+        response = await fetch(
+            "https://api-inference.huggingface.co/models/StevenLimcorn/indonesian-roberta-base-emotion-classifier?wait_for_model=true",
+            {
+                headers: { Authorization: `Bearer ${process.env.API_KEY}` },
+                method: "POST",
+                body: JSON.stringify(text),
+            }
+        );
+
+        if (response.status == 200) {
+            response = await response.json()
+            res.statusCode = 200;
+
+            result = "";
+            let highestScore = 0;
+            response[0].forEach(({ label, score }) => {
+                if (score > highestScore) {
+                    highestScore = score;
+                    result = label;
+                }
+            })
+        }
+    } else {
+        response = { 'error': 'Bad request' }
+        res.statusCode = 400;
+    }
+    // Send the JSON response
+    res.end(result);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at ${hostname}:${port}`);
+});
